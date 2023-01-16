ebb533a6912db119f3e7a1e8a1cbbe9d81f2442c
Remezov Dmitriy <darknessdizi@gmail.com>
2023-01-16 23:28:40 +0700

    Add functions is_empty, pop, peek

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 class Stack:
 
-    def __init__(self, text):
-        
-        self.string = text
+    def __init__(self, string):
+
+        self.string = string[::-1]
+        self.list = '[]'
+        self.dict = '{}'
+        self.tuple = '()'
 
     def is_empty(self) -> bool:
+
+        '''Проверка стека на пустоту. Метод возвращает True или False.'''
 
         if self.string:
             return True
@@ -12,15 +17,55 @@ class Stack:
             return False
 
     def push(self) -> None:
+
+        '''Добавляет новый элемент на вершину стека. Метод ничего не возвращает.'''
+
         pass
 
     def pop(self) -> str:
-        pass
+
+        '''Удаляет верхний элемент стека. Стек изменяется. Метод возвращает верхний элемент стека.'''
+
+        print(self.string[::-1])
+        print('В классе: ', self.string)
+        for index, element in enumerate(self.string):
+            if index == len(self.string) - 1:
+                break
+            if index == 0:
+                continue
+            element_stack = element + self.string[index-1]
+
+            if (element_stack == self.list or
+            element_stack == self.dict or
+            element_stack == self.tuple):
+                end = index - 1
+                begin = index + 1
+                self.string = self.string[:end] + self.string[begin:]
+                print('В классе удаление: ', self.string)
+                return element_stack
 
     def peek(self) -> str:
-        pass
+        
+        '''Возвращает верхний элемент стека, но не удаляет его. Стек не меняется.'''
+
+        for index, element in enumerate(self.string):
+            if index == len(self.string) - 1:
+                break
+            if index == 0:
+                continue
+            element_stack = element + self.string[index-1]
+
+            if (element_stack == self.list or
+            element_stack == self.dict or
+            element_stack == self.tuple):
+                return element_stack
+            
+             
 
     def size(self) -> int:
+        
+        '''Возвращает количество элементов в стеке.'''
+
         pass
 
 
@@ -37,5 +82,14 @@ list_of_strings = [
 
 if __name__ == '__main__':
     a = Stack(list_of_strings[0])
-    b = Stack(list_of_strings[1])
+    b = Stack(list_of_strings[0])
+    c = Stack(list_of_strings[1])
+    d = Stack(list_of_strings[2])
     print(a.is_empty(), b.is_empty())
+
+    print(b.peek())
+    print(c.peek())
+    print(d.peek())
+
+    print(c.pop())
+    print(c.pop())

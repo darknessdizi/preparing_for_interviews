ed8d5561ceed361df061ccc3c28c740b6fd14918
Remezov Dmitriy <darknessdizi@gmail.com>
2023-01-17 00:38:57 +0700

    Add function size

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ class Stack:
     def __init__(self, string):
 
         self.string = string[::-1]
-        self.list = '[]'
-        self.dict = '{}'
-        self.tuple = '()'
 
     def is_empty(self) -> bool:
 
@@ -22,22 +19,21 @@ class Stack:
 
         pass
 
+    def _element_search(self):
+        pass
+
     def pop(self) -> str:
 
         '''Удаляет верхний элемент стека. Стек изменяется. Метод возвращает верхний элемент стека.'''
 
         print(self.string[::-1])
-        print('В классе: ', self.string)
+        print('Объект класса: ', self.string)
         for index, element in enumerate(self.string):
-            if index == len(self.string) - 1:
-                break
             if index == 0:
                 continue
             element_stack = element + self.string[index-1]
 
-            if (element_stack == self.list or
-            element_stack == self.dict or
-            element_stack == self.tuple):
+            if element_stack in ['{}', '[]', '()']:
                 end = index - 1
                 begin = index + 1
                 self.string = self.string[:end] + self.string[begin:]
@@ -55,18 +51,19 @@ class Stack:
                 continue
             element_stack = element + self.string[index-1]
 
-            if (element_stack == self.list or
-            element_stack == self.dict or
-            element_stack == self.tuple):
+            if element_stack in ['{}', '[]', '()']:
                 return element_stack
             
-             
-
     def size(self) -> int:
         
         '''Возвращает количество элементов в стеке.'''
 
-        pass
+        count_tuple = self.string.count('(') + self.string.count(')')
+        count_list = self.string.count('[') + self.string.count(']')
+        count_dict = self.string.count('{') + self.string.count('}')
+        if count_tuple % 2 == 0 and count_list % 2 == 0 and count_dict % 2 == 0:
+            count = int((count_tuple + count_list + count_dict) / 2)
+            return count
 
 
 list_of_strings = [
@@ -83,13 +80,22 @@ list_of_strings = [
 if __name__ == '__main__':
     a = Stack(list_of_strings[0])
     b = Stack(list_of_strings[0])
-    c = Stack(list_of_strings[1])
+    c = Stack(list_of_strings[4])
     d = Stack(list_of_strings[2])
-    print(a.is_empty(), b.is_empty())
+    #print(a.is_empty(), b.is_empty())
 
-    print(b.peek())
+    '''print(b.peek())
     print(c.peek())
     print(d.peek())
 
     print(c.pop())
     print(c.pop())
+    print(c.pop())'''
+    print(c.size())
+    print(c.pop())
+    print(c.pop())
+    print(c.pop())
+    print(c.pop())
+    print(c.pop())
+    print(c.pop())
+    print(c.size())
